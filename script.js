7e8f112a62cd9c484ca39be730db8012913c5a10
Mubashira <mubisaifu2017@gmail.com>
2021-12-30 01:29:45 +0530

    initial

---
+++ script.js
@@ -0,0 +1,20 @@
+class Film{
+
+    
+    book(){
+    window.location.href="./book.html"
+}
+
+
+
+payment(){
+     
+    swal("Thank you", "Booking has been confirmed", "success");
+
+    
+ }
+
+}
+
+var movie = new Film()
+
